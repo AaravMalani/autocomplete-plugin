483c3ad0f5822a7cc4663c1fd0b7dbd225160219
Aarav Malani <aarav.malani@gmail.com>
2026-03-19 00:29:07 +0800

    Initial commit

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+import nodeResolve from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
+import typescript from "@rollup/plugin-typescript";
+
+/**
+ * @type {import('rollup').RollupOptions}
+ */
+export default ["web", "runner"].map(target => ({
+  plugins: [nodeResolve(), typescript()],
+  input: `src/${target}.ts`,
+  output: {
+    plugins: [terser()],
+    dir: "dist",
+    format: "iife",
+    name: "plugin",
+    sourcemap: true,
+  },
+}));
